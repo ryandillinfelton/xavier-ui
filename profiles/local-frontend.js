1708d960e98fe348b022c04f8dd69eb71237bef9
Ryan Felton <rfelton@redhat.com>
2019-08-22 12:57:49 -0400

    updated local-frontend routes

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -1,13 +1,15 @@
 /*global module*/
 
-const SECTION = 'staging';
+const SECTION = 'migrations';
 const APP_ID = 'migration-analytics';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
 routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}`]                = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
+routes['/config']                    = { host: `http://localhost:${FRONTEND_PORT}` };
 
 module.exports = { routes };
