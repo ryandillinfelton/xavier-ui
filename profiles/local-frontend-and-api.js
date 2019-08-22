210b9c6796fcc16ceb6a2bff5c73a154423fd70c
Ryan Felton <rfelton@redhat.com>
2019-08-22 12:13:00 -0400

    changed spandx config to work for new bundle

--- profiles/local-frontend-and-api.js
+++ profiles/local-frontend-and-api.js
@@ -1,6 +1,6 @@
 /*global module*/
 
-const SECTION = 'staging';
+const SECTION = 'migrations';
 const APP_ID = 'migration-analytics';
 const FRONTEND_PORT = 8002;
 const API_PORT = 8080;
@@ -9,8 +9,10 @@ const routes = {};
 
 routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}`]                = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
 routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
+routes['/config']                    = { host: `https://localhost:${FRONTEND_PORT}` };
 
 // routes[`/api/xavier`] = { host: `http://localhost:${API_PORT}` };
 routes[`/api/xavier`] = { host: `http://analytics-integration-migration-analytics.127.0.0.1.nip.io` };
